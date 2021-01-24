2d18552df88d06a60afbecd3beb52c7fe83d0c1b
MAISDUSE <50581556+MAISDUSE@users.noreply.github.com>
2021-01-24 17:04:15 +0100

    View render en une commande + tests dans la vue

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -8,12 +8,17 @@ class IndexController extends Controller
 {
     public function index()
     {
-        $data = [];
-        /*return $this->view->view("layout/app.layout.php", [
-            "param1" => $data
-        ]);*/
+        $data1 = "Ceci est ma première donnée via render";
+        $data2 = [1,2,3,4,5,6,7,8,9];
 
-        return $this->view->display("layout/app.layout.php");
+        return $this->view->render("layout/app.layout.php", [
+            "param1" => $data1, //variable simple
+            "param2" => $data2 //tableau entier
+        ]);
+
+
+        //return $this->view->display("layout/app.layout.php");
+        //ancienne methode desomais on fait tout en un appel de render
     }
 
     public function contact()
