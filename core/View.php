aa38beb3cc66dbb3358d70b8d8350478e5421e99
Hugo Pontacq <hpontacq@gmail.com>
2021-01-24 17:35:53 +0100

    file formatting

--- core/View.php
+++ core/View.php
@@ -7,63 +7,66 @@ namespace core;
 
 class View
 {
-  //Paramètres de la vue, dans un tableau associatif
-  private array $param;
-  private const VIEW_FOLDER = __DIR__."/../resources/views/";
+    //Paramètres de la vue, dans un tableau associatif
+    private array $param;
+    private const VIEW_FOLDER = __DIR__ . "/../resources/views/";
 
 
-  //constructeur d'une vue
-  function __construct(){
-    //initialise un tableau vide de Paramètres
-    $this->param = array();
-  }
-
-  // Ajoute une variable à la vue
-  function assign(string $varName,$value) {
-    $this->param[$varName] = $value;
-  }
-
-  // Affiche la vue
-  function display(string $filename) {
-
-    // Ajoute le chemin relatif vers le fichier de la vue
-    $p = self::VIEW_FOLDER.$filename;
-
-    // Tous les attributs de l'objet sont dupliqués en des variables
-    // locales à la fonction display. Cela simplifie l'expression des
-    // valeurs de la vue. Il faut simplement utiliser <?= $variable
-
-    // Parcourt toutes les variables de la vue
-    foreach ($this->param as $key => $value) {
-      // La notation $$ dédigne une variable dont le nom est dans une autre variable
-      $$key = $value;
-      // Création d'autant de variables locales qu'il y a d'atributs publics dans $this
+    //constructeur d'une vue
+    function __construct()
+    {
+        //initialise un tableau vide de Paramètres
+        $this->param = array();
     }
 
-    // Inclusion de la vue
-    // Comme cette inclusion est dans la portée de la méthode show alors
-    // seules les variables locales à show sont visibles.
-    return include($p);
-    // A l'intérieur donc local a la methode show()
-  }
+    // Ajoute une variable à la vue
+    function assign(string $varName, $value)
+    {
+        $this->param[$varName] = $value;
+    }
+
+    // Affiche la vue
+    function display(string $filename)
+    {
+        // Ajoute le chemin relatif vers le fichier de la vue
+        $p = self::VIEW_FOLDER . $filename;
+
+        // Tous les attributs de l'objet sont dupliqués en des variables
+        // locales à la fonction display. Cela simplifie l'expression des
+        // valeurs de la vue. Il faut simplement utiliser <?= $variable
+
+        // Parcourt toutes les variables de la vue
+        foreach ($this->param as $key => $value)
+        {
+            // La notation $$ dédigne une variable dont le nom est dans une autre variable
+            $$key = $value;
+            // Création d'autant de variables locales qu'il y a d'atributs publics dans $this
+        }
+
+        // Inclusion de la vue
+        // Comme cette inclusion est dans la portée de la méthode show alors
+        // seules les variables locales à show sont visibles.
+        return include($p); // A l'intérieur donc local a la methode show()
+    }
 
     // Affiche toutes les valeurs des paramètres de la vue
-  function dump() {
-    foreach ($this->param as $key => $value) {
-      print("<br/><b>$key: </b>\n");
-      var_dump($value);
+    function dump()
+    {
+        foreach ($this->param as $key => $value)
+        {
+            print("<br/><b>$key: </b>\n");
+            var_dump($value);
+        }
     }
-  }
 
-  //Fonction qui fait tout en un : assigne un tableau de paramètres et ensuite display la vue.
-  function render(string $filename, $param=array()){
-    foreach ($param as $key => $value) {
-    $this->assign($key, $value);
+    //Fonction qui fait tout en un : assigne un tableau de paramètres et ensuite display la vue.
+    function render(string $filename, $param = array())
+    {
+        foreach ($param as $key => $value)
+        {
+            $this->assign($key, $value);
+        }
+
+        return $this->display($filename);
     }
-    $this->display($filename);
-
-  }
-
-
-
 }
