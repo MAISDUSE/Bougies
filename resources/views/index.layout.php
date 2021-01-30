1d63e8e6f34a5d788cb011acd35c99908c0e3760
MAISDUSE <50581556+MAISDUSE@users.noreply.github.com>
2021-01-30 22:26:11 +0100

    Suppression bouton refresh chart

--- resources/views/index.layout.php
+++ resources/views/index.layout.php
@@ -86,9 +86,6 @@
                         <button type="button" class="btn btn-tool" data-card-widget="collapse">
                             <i class="fas fa-minus"></i>
                         </button>
-                        <button type="button" class="btn btn-tool" id="refreshChart">
-                            <i class="fas fa-sync-alt"></i>
-                        </button>
                     </div>
                 </div>
                 <div class="card-body">
